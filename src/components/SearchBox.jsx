e661f49e820fd52677dc411d69653682b84f7ddc
Ashley Fox <admin@foxonlinesolutions.co.uk>
2017-11-27 17:10:56 +0000

    #12: Limit maximum search characters to 250.

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -15,6 +15,9 @@ type Props = {
 // Extend the Searchkit SearchBox component to redirect on change.
 class SearchBox extends SearchkitSearchBox<Props> {
   onChange(event: any): void {
+    if (event.target.value.length > 250) {
+      return;
+    }
     const {pathname, push} = this.props;
     if (pathname !== '/') {
       push('/');
