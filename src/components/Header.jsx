97d23cfcc4afc16d28d8065f81b24aa4f67f362a
Julien Le Hericy <j.lehericy@cessda.eu>
2018-04-04 08:43:59 +0200

    Remove Beta logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,7 +48,7 @@ class Header extends Component<Props> {
 
     return (
       <header>
-        <span className="beta">Beta</span>
+        /*<span className="beta">Beta</span>*/
 
         <div className="container">
           <div className="cessda-organisation"><Translate content="cessda"/></div>
