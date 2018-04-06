272930a3c6d85f01df55bb3df2eaedc893023346
Julien Le Hericy <j.lehericy@cessda.eu>
2018-04-06 15:53:56 +0200

    Fix merge issue

--- infrastructure/gcp/pasc-searchkit-registration.sh
+++ infrastructure/gcp/pasc-searchkit-registration.sh
@@ -17,18 +17,10 @@ source ./gcp.config > /dev/null 2>&1
 gcloud container clusters get-credentials $PRODUCT-$ENVIRONMENT-cc --zone=$ZONE > /dev/null 2>&1
 
 ### Kubernetes Deployment ###
-<<<<<<< HEAD
-if kubectl get rc $PRODUCT-es-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
-=======
 if kubectl get deployment $PRODUCT-es-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
->>>>>>> dev
   then
     echo "ElasticSearch component available, deployment will be processed"
   else
     echo "ElasticSearch component not available, deployment's aborted"
     exit 1
 fi;
-<<<<<<< HEAD
-
-=======
->>>>>>> dev
