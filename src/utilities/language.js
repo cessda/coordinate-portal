b6b424e39830a59df1979d60e82f351609d6662b
John Shepherdson <john.shepherdson@gmail.com>
2018-11-05 18:00:03 +0000

    Added entry for all 8 available languages. Note that only 5 have an matching ES index at present

--- src/utilities/language.js
+++ src/utilities/language.js
@@ -1,10 +1,13 @@
 // @flow
 
-import en from '../locales/en';
-import de from '../locales/de';
+import en from '../locales/de';
+import de from '../locales/en';
 import fi from '../locales/fi';
+import fi from '../locales/fr';
+//import fi from '../locales/nl';
 import se from '../locales/se';
 import sk from '../locales/sk';
+//import fi from '../locales/sl';
 
 export function getLanguages(): Object[] {
   // Register translations stored in the "/locales" directory by adding them to the array below.
@@ -16,20 +19,30 @@ export function getLanguages(): Object[] {
   //   locale : The imported locale for this language.
   // }
   return [{
-    code: 'en',
-    label: 'English',
-    index: 'cmmstudy_en',
-    locale: en
-  }, {
     code: 'de',
     label: 'Deutsch',
     index: 'cmmstudy_de',
     locale: de
   }, {
+    code: 'en',
+    label: 'English',
+    index: 'cmmstudy_en',
+    locale: en
+  }, {
     code: 'fi',
     label: 'Suomi',
     index: 'cmmstudy_fi',
     locale: fi
+  }, {
+    code: 'fr',
+    label: 'Francais',
+    index: 'cmmstudy_fr', 
+    locale: fr
+  }, {
+    code: 'nl',
+    label: 'Dutch',
+    index: 'cmmstudy_nl',
+    locale: nl
   }, {
     code: 'se',
     label: 'Svenska',
@@ -37,8 +50,13 @@ export function getLanguages(): Object[] {
     locale: se
   }, {
     code: 'sk',
-    label: 'SlovenskÃ½',
+    label: 'Slovenský',
     index: 'cmmstudy_en', // TODO : Change to 'cmmstudy_sk' when Elasticsearch index is ready.
     locale: sk
+  }, {
+    code: 'sl',
+    label: 'Francais',
+    index: 'cmmstudy_en', // TODO : Change to 'cmmstudy_sl' when Elasticsearch index is ready.
+    locale: sl
   }];
 }
