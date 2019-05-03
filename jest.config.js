cde3fca08f8ac0cd7379a4c331e542eb2ed6989f
Matthew Morris <matthew.morris@cessda.eu>
2019-05-03 14:18:26 +0200

    Show test results in Jenkins

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testMatch: ['**/tests/**/*.js'],
   coverageDirectory: './coverage',
-  collectCoverage: true
+  collectCoverage: true,
+  reporters: [ "default", "jest-junit" ]
 };
