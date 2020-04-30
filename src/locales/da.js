521eac0321b243a38ba3da5e733d01ac39119572
John Shepherdson <john.shepherdson@cessda.eu>
2020-04-30 15:34:56 +0100

    Reversed A-Z and Z-A labels. See #154

--- src/locales/da.js
+++ src/locales/da.js
@@ -79,8 +79,8 @@ module.exports = {
   },
   sorting: {
     relevance: 'Relevance',
-    titleAscending: 'Title (Z - A)',
-    titleDescending: 'Title (A - Z)',
+    titleAcscending: 'Title (A - Z)',
+    titleDescending: 'Title (Z - A)',
     dateAscending: 'Date of collection (oldest)',
     dateDescending: 'Date of collection (newest)'
   },
