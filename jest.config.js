0b70d6c3757c9dbf6595ec6bec997a8f83f4bcda
Matthew Morris <matthew.morris@cessda.eu>
2022-01-31 15:41:14 +0100

    #394: Exclude mock data from being tested.

--- jest.config.js
+++ jest.config.js
@@ -28,5 +28,6 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>setupTests.js"],
   testEnvironment: "jsdom",
 
-  testMatch: ["**/tests/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"]
+  testMatch: ["**/tests/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/", "/tests/mockdata.ts"]
 };
