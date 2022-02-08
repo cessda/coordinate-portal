bdba4fc96a98524fdd36d9728e5d50767f82c3d3
kpapag <papagiannopoulos.konstantinos@outlook.com>
2022-02-08 17:40:01 +0200

    #393-metrics-for-prom-tests-prod

--- server/server.prod.ts
+++ server/server.prod.ts
@@ -10,9 +10,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import express from 'express';
+import express, {Request, Response} from 'express';
 import path from 'path';
-import { checkBuildDirectory, checkEnvironmentVariables, startListening } from './helper';
+import { checkBuildDirectory, checkEnvironmentVariables, startListening, restResponseTimeAllHistogram, restResponseTimeLangHistogram, restResponseTimePublisherHistogram } from './helper';
+import responseTime from 'response-time'
 
 export function start () {
     checkBuildDirectory();
@@ -21,6 +22,39 @@ export function start () {
     const app = express();
 
     app.use('/static', express.static(path.join(__dirname, '../dist')));
+    
+    //Metrics middleware for API
+    app.use('/api/DataSets', responseTime((req:Request, res:Response, time:number)=>{
+      //ALL
+      if (req?.route?.path){
+        restResponseTimeAllHistogram.observe({
+          method: req.method,
+          route: req.route.path,
+          status_code: res.statusCode
+        }, Math.round(time*1000))
+      }
+      //LANG
+      if (req.query.metadataLanguage){
+        restResponseTimeLangHistogram.observe({
+          method: req.method,
+          route: req.route.path,
+          lang: req.query.metadataLanguage as string,
+          status_code: res.statusCode
+        }, Math.round(time*1000))
+      }
+      //PUBLISHER
+      if (req.query.publishers){
+        let publishers:any = req.query.publishers
+        publishers.forEach(function (value: any) {
+          restResponseTimePublisherHistogram.observe({
+            method: req.method,
+            route: req.route.path,
+            publ: value as string,
+            status_code: res.statusCode
+          }, Math.round(time*1000))
+        });
+      }
+    }))
 
     const indexPath = path.join(path.join(__dirname, '../dist'), 'index.html');
 
