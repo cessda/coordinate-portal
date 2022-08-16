910a80a6436b6b43e190713a16a37d6cdc91c23c
Matthew Morris <matthew.morris@cessda.eu>
2022-08-16 13:28:40 +0200

    #336: Add tests for rendering the HTML and content negotiation

---
+++ tests/server/helper.ts
@@ -0,0 +1,162 @@
+/** @jest-environment node */
+// Copyright CESSDA ERIC 2017-2022
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Elasticsearch from '../../server/elasticsearch';
+import { ApiResponse, Client } from '@elastic/elasticsearch/api/new';
+import httpMocks from 'node-mocks-http';
+import { mockStudy } from '../common/mockdata';
+import { getJsonLd, getStudyModel } from '../../common/metadata';
+import { ResponseError } from '@elastic/elasticsearch/lib/errors';
+
+// Constants
+const ejsTemplate = "server/views/index.ejs";
+const requestParameters = {
+  path: "/detail",
+  query: { q: "test" }
+};
+
+// Mock setup
+jest.mock('../../server/elasticsearch');
+const mockedElasticsearch = Elasticsearch as jest.MockedClass<typeof Elasticsearch>;
+const mockedGetStudy = jest.fn<ReturnType<InstanceType<typeof Elasticsearch>["getStudy"]>, never>();
+mockedElasticsearch.mockImplementation(() => {
+  return {
+    client: jest.fn() as unknown as Client,
+    getStudy: mockedGetStudy,
+    getSimilars: jest.fn(),
+    getTotalStudies: jest.fn()
+  }
+});
+
+// Import the helper
+import { Metadata, renderResponse } from '../../server/helper';
+
+// Reset the mock after each test
+beforeEach(() => mockedGetStudy.mockReset());
+
+describe('helper utilities', () => {
+  describe('renderResponse()', () => {
+    it('should return a rendered response', async () => {
+      const request = httpMocks.createRequest();
+      const response = httpMocks.createResponse();
+
+      await renderResponse(request, response, ejsTemplate);
+
+      // Status should be 200, and the template should be set correctly.
+      expect(response.statusCode).toBe(200);
+      expect(response._getRenderView()).toBe(ejsTemplate);
+      expect(response._getRenderData()).toEqual({ metadata: {}});
+    });
+
+    it('should return 406 when a client requests JSON-LD on pages that are not the detail page', async () => {
+      const request = httpMocks.createRequest({
+        headers: {
+          accept: "application/ld+json"
+        },
+        path: "/"
+      });
+      const response = httpMocks.createResponse();
+
+      await renderResponse(request, response, ejsTemplate);
+
+      // Status code should be 406
+      expect(response.statusCode).toBe(406);
+    });
+
+    it('should return 406 when a client requests an unsupported content type', async () => {
+      const request = httpMocks.createRequest({
+        headers: {
+          accept: "application/ld+json"
+        },
+        ...requestParameters
+      });
+      const response = httpMocks.createResponse();
+
+      await renderResponse(request, response, ejsTemplate);
+
+      // Status code should be 406
+      expect(response.statusCode).toBe(406);
+    });
+
+    it('should request metadata', async () => {
+      const request = httpMocks.createRequest(requestParameters);
+      const response = httpMocks.createResponse();
+
+      mockedGetStudy.mockResolvedValue(mockStudy);
+      await renderResponse(request, response, ejsTemplate);
+
+      // Status code should be 200
+      expect(response.statusCode).toBe(200);
+      expect(mockedGetStudy).toBeCalledWith("test", "cmmstudy_en");
+      
+      // Assert fields of renderData are as expected
+      const renderData = response._getRenderData() as Metadata;
+      expect(renderData).toEqual({
+        metadata: {
+          creators: mockStudy.creators.join('; '),
+          description: mockStudy.abstractShort,
+          title: mockStudy.titleStudy,
+          publisher: mockStudy.publisher.publisher,
+          jsonLd: getJsonLd(getStudyModel({ _source: mockStudy }))
+        }
+      });
+    });
+
+    it('should return 503 on Elasticsearch error', async () => {
+      const request = httpMocks.createRequest(requestParameters);
+      const response = httpMocks.createResponse();
+
+      // Simulate Elasticsearch returning 500
+      mockedGetStudy.mockRejectedValue(new ResponseError({ statusCode: 500 } as ApiResponse));
+
+      await renderResponse(request, response, ejsTemplate);
+
+      // Status code should be 503
+      expect(response.statusCode).toBe(503);
+      expect(mockedGetStudy).toBeCalledWith("test", "cmmstudy_en");
+      expect(response._getRenderData()).toEqual({ metadata: {}});
+    });
+
+    it('should return 404 when Elasticsearch returns 404', async () => {
+
+      const request = httpMocks.createRequest(requestParameters);
+      const response = httpMocks.createResponse();
+
+      // Simulate Elasticsearch returning 500
+      mockedGetStudy.mockRejectedValue(new ResponseError({ statusCode: 404 } as ApiResponse));
+
+      await renderResponse(request, response, ejsTemplate);
+
+      // Status code should be 404
+      expect(response.statusCode).toBe(404);
+      expect(mockedGetStudy).toBeCalledWith("test", "cmmstudy_en");
+      expect(response._getRenderData()).toEqual({ metadata: {}});
+    });
+
+    it('should return 404 when Elasticsearch returns nothing', async () => {
+      const request = httpMocks.createRequest(requestParameters);
+      const response = httpMocks.createResponse();
+
+      // Simulate Elasticsearch returning nothing
+      mockedGetStudy.mockResolvedValue(undefined);
+
+      await renderResponse(request, response, ejsTemplate);
+
+      // Status code should be 404
+      expect(response.statusCode).toBe(404);
+      expect(mockedGetStudy).toBeCalledWith("test", "cmmstudy_en");
+      expect(response._getRenderData()).toEqual({ metadata: {}});
+    });
+  });
+});
