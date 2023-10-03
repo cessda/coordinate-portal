f11175423cf6c7a66e673e6c473a21ff25ddb8c1
Matthew Morris <matthew.morris@cessda.eu>
2023-10-03 10:37:24 +0200

    Only keep dependencies needed to run the Searchkit server in production dependencies

--- server/metrics.ts
+++ server/metrics.ts
@@ -10,7 +10,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { URL, URLSearchParams } from 'url'
+import { URL, URLSearchParams } from 'url';
 import client from 'prom-client';
 import { Request, RequestHandler, Response } from 'express';
 import Elasticsearch from './elasticsearch';
