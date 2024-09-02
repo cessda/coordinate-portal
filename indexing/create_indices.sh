60c1a50b270295c8ddb2a2efea174b2b1979634d
Markus Tuominen <markus.tuominen@tuni.fi>
2024-09-02 12:26:34 +0300

    Add reindex queries and scripts, update creators, improve tooltip, fix overflows (#11)

---
+++ indexing/create_indices.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Function to create index from settings JSON file
+create_index() {
+    local index_name="$1"
+    local settings_file="$2"
+    local api_key=""
+
+    curl -X PUT "localhost:9200/$index_name?pretty" \
+    -H "Authorization: ApiKey $api_key" \
+    -H 'Content-Type: application/json' \
+    -d @"$settings_file"
+}
+
+# Loop through settings JSON files and create indices
+for settings_file in settings/*.json; do
+    filename=$(basename -- "$settings_file")
+    lang=$(echo "$filename" | cut -d "_" -f 3 | cut -d "." -f 1)
+    create_index "coordinate_$lang" "$settings_file"
+done
--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/display-name */
 // Copyright CESSDA ERIC 2017-2024
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -12,35 +11,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React, {useState, useRef} from 'react';
-import {FaQuestionCircle} from 'react-icons/fa';
+import React, { useState, useRef } from 'react';
+import { FaQuestionCircle } from 'react-icons/fa';
 
 interface TooltipProps {
   content: string;
   id?: string;
-  classNames?: {container?: string, button?: string, content?: string, item?: string};
+  classNames?: { container?: string, button?: string, content?: string, item?: string };
   ariaLabel?: string;
 }
 
 export default ({ content, id, classNames, ariaLabel }: TooltipProps) => {
   const [isActive, setIsActive] = useState(false);
+  const [isNearBottom, setIsNearBottom] = useState(false);
   const tooltipButtonRef = useRef<HTMLButtonElement>(null);
+  const minDistanceFromBottom = 200;
 
-  const handleKeyDown = (event: React.KeyboardEvent) => {
-    if (event.key === 'Escape' && isActive) {
-      event.preventDefault();
-      event.stopPropagation();
-      if (tooltipButtonRef.current) {
-        tooltipButtonRef.current.click();
-      } else {
-        setIsActive(false);
-      }
+  const calculatePosition = () => {
+    if (tooltipButtonRef.current) {
+      const rect = tooltipButtonRef.current.getBoundingClientRect();
+      const nearBottom = window.innerHeight - rect.bottom < minDistanceFromBottom;
+      setIsNearBottom(nearBottom);
     }
   };
 
   const handleClick = (event: React.MouseEvent) => {
     event.preventDefault();
     event.stopPropagation();
+
+    if (!isActive) {
+      calculatePosition();
+    }
     setIsActive(!isActive);
   };
 
@@ -48,27 +49,47 @@ export default ({ content, id, classNames, ariaLabel }: TooltipProps) => {
   // Mouse click to toggle tooltip
   // Hover to open tooltip
   // Unfocus, Unhover or Escape to close tooltip
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Escape' && isActive) {
+      event.preventDefault();
+      event.stopPropagation();
+      setIsActive(false);
+    }
+  };
+
   return (
-    <div className={`dropdown is-right ${classNames?.container}${isActive ? ' is-active' : ''}`}
-         onBlur={() => setIsActive(false)}
-         onMouseEnter={() => setIsActive(true)}
-         onMouseLeave={() => setIsActive(false)}>
+    <div
+      className={`dropdown is-right ${classNames?.container}${isActive ? ' is-active' : ''}`}
+      onBlur={() => setIsActive(false)}
+      onMouseEnter={() => { calculatePosition(); setIsActive(true); }}
+      onMouseLeave={() => setIsActive(false)}
+    >
       <div className="dropdown-trigger">
-        <button ref={tooltipButtonRef} className={`button focus-visible ${classNames?.button}`} aria-haspopup="true"
-                {...(isActive ? {'aria-describedby': id} : {'aria-label': ariaLabel})}
-                onClick={(e) => handleClick(e)}
-                onKeyDown={(e) => handleKeyDown(e)}>
-          <FaQuestionCircle className="tooltip-icon" aria-hidden="true"/>
+        <button
+          ref={tooltipButtonRef}
+          className={`button focus-visible ${classNames?.button}`}
+          aria-haspopup="true"
+          {...(isActive ? { 'aria-describedby': id } : { 'aria-label': ariaLabel })}
+          onClick={(e) => handleClick(e)}
+          onKeyDown={(e) => handleKeyDown(e)}
+        >
+          <FaQuestionCircle className="tooltip-icon" aria-hidden="true" />
         </button>
       </div>
-      <div className="dropdown-menu">
-        <div className={`dropdown-content ${classNames?.content}`}>
-          <div id={id} className={`dropdown-item ${classNames?.item}`} role="tooltip"
-               aria-hidden={isActive ? 'false' : 'true'}>
-            <p dangerouslySetInnerHTML={{ __html: content }} />
+      {isActive && (
+        <div className={`dropdown-menu ${isNearBottom ? ' tooltip-above' : ''}`}>
+          <div className={`dropdown-content ${classNames?.content}`}>
+            <div
+              id={id}
+              className={`dropdown-item ${classNames?.item}`}
+              role="tooltip"
+              aria-hidden={isActive ? 'false' : 'true'}
+            >
+              <p dangerouslySetInnerHTML={{ __html: content }} />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
-}
+};
