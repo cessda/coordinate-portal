2d83dde988ad8c7b8b04f4eea9b5716efdbd59d0
Matthew Morris <matthew.morris@cessda.eu>
2025-05-12 14:14:49 +0200

    Update copyright headers for 2025

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-// Copyright CESSDA ERIC 2017-2024
+/* eslint-disable @typescript-eslint/no-require-imports */
+// Copyright CESSDA ERIC 2017-2025
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License.
